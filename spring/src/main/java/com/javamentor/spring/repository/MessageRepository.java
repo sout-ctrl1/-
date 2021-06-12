3af3690b80b7103d99e496f924398c3ee2954b46
sout-ctrl1 <75034017+sout-ctrl1@users.noreply.github.com>
2021-06-12 20:01:01 +0300

    Delete spring directory

--- spring/src/main/java/com/javamentor/spring/repository/MessageRepository.java
+++
@@ -1,6 +0,0 @@
-package com.javamentor.spring.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface MessageRepository extends CrudRepository<String, Long> {
-}
